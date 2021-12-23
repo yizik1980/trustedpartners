0b2fd1773d80bf9371b0cfd0c58ae6eb9397c7bd
yizik1980 <syizikm@gmail.com>
2021-12-23 11:18:42 +0200

    minor fixes nainly style

--- trusted-partners/src/app/components/search-box/search-box.component.ts
+++ trusted-partners/src/app/components/search-box/search-box.component.ts
@@ -1,30 +1,32 @@
 import { AfterViewInit, Component, ElementRef, OnChanges, OnInit, SimpleChange, SimpleChanges, ViewChild } from '@angular/core';
 import { debounceTime, filter, fromEvent, mergeMap } from 'rxjs';
 import { RemoteInfoService } from 'src/app/services/remote-info.service';
-import  info  from '../../model/info';
+import info from '../../model/info';
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.scss']
 })
-export class SearchBoxComponent implements OnInit ,AfterViewInit{
+export class SearchBoxComponent implements OnInit, AfterViewInit {
   @ViewChild('searchField') searchField: ElementRef = new ElementRef(null);
-  info:info | undefined;
-  constructor(private remoteInfo:RemoteInfoService) { }
+  info: info | undefined;
+  errorMessage = '';
+  constructor(private remoteInfo: RemoteInfoService) { }
   ngOnInit(): void {
   }
   ngAfterViewInit(): void {
-    if(this.searchField){
-      fromEvent<KeyboardEvent>(this.searchField.nativeElement,'keydown')
-      .pipe(debounceTime(2000))
-       .pipe(mergeMap(keyEvent=>{
-        var txtVal = (keyEvent.target as HTMLInputElement).value;
-        console.log(txtVal);
-         return this.remoteInfo.getInfo(txtVal)
-       })).subscribe(result=>{
-         this.info = result;
-         console.log(result);
-       });
+    if (this.searchField) {
+      fromEvent<KeyboardEvent>(this.searchField.nativeElement, 'keydown')
+        .pipe(debounceTime(1000))
+        .pipe(mergeMap(keyEvent => {
+          var txtVal = (keyEvent.target as HTMLInputElement).value;
+          console.log(txtVal);
+          return this.remoteInfo.getInfo(txtVal)
+        })).subscribe(result => {
+          this.info = result;
+          console.log(result);
+          this.errorMessage = result.RelatedTopics.length > 0 ? '' : 'nothing found to ' + result.Definition
+        });
     }
   }
 
