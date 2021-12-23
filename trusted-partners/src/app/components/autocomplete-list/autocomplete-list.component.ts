8901e60a74c5e4eb0dae032eec166f48bbf3ced8
yizik1980 <syizikm@gmail.com>
2021-12-23 12:14:04 +0200

    paging main searches

--- trusted-partners/src/app/components/autocomplete-list/autocomplete-list.component.ts
+++ trusted-partners/src/app/components/autocomplete-list/autocomplete-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { RelatedTopic, Topic } from 'src/app/model/info';
+import { pages } from 'src/app/model/pages';
 
 @Component({
   selector: 'app-autocomplete-list',
@@ -11,11 +12,19 @@ export class AutocompleteListComponent implements OnChanges {
   relatedTopics: RelatedTopic[] | undefined;
   mainTopics:Topic[] | undefined;
   subTopics:RelatedTopic[] | undefined;
+  mainPage:pages = {
+    pageSize : 4,
+    page : 1,
+    mainLength : 0
+  };
+
+
   constructor() { }
 
   ngOnChanges(): void {
     if(this.relatedTopics && this.relatedTopics.length>0){
       this.mainTopics = this.relatedTopics?.filter(item=>item.FirstURL);
+      this.mainPage.mainLength = this.mainTopics.length;
       this.subTopics = this.relatedTopics?.filter(item=>item.Name);
     }
   }
---
+++ trusted-partners/src/app/model/pages.ts
@@ -0,0 +1,5 @@
+export interface pages {
+    pageSize: number;
+    page: number;
+    mainLength: number;
+}
